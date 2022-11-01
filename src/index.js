71f55566e6e3ee029bdd60dedfa99814d81baf5c
Gabriel <gabriel.fernandesb@gmail.com>
2022-11-01 13:30:23 -0300

    feat: patch todos/:id/done

--- src/index.js
+++ src/index.js
@@ -90,7 +90,20 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { id } = request.params
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if (!todo) {
+    return response.status(400).json({
+      error: 'Todo not found!'
+    })
+  }
+
+  todo.done = true
+
+  return response.status(200).send()
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
